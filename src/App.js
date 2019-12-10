a0590a3c2fbfc885b4478d016f814e63732c1255
Mia Wong <miaw629@gmail.com>
2019-12-09 19:07:38 -0500

    added sidebar and css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="sidebar">
+                <li>
+                    <a href="/" className="home">
+                        HOME
+                    </a>
+                </li>
+                <li>Atlanta, GA</li>
+                <li>Austin, TX </li>
+                <li>New York, NY </li>
+            </div>
+            <div className="content">
+                <h1>
+                    <input type="text" className="searchBar" />
+                </h1>
+                <h3>LOCATION</h3>
+                <h3>Date</h3>
+                <h3>Temperature </h3>
+                <h3>Summary</h3>
+            </div>
+        </div>
+    );
 }
 
 export default App;
